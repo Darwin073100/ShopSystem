1d70eda28107b736ab3f3bfdf1327d6a31d724d1
Edwin Garcia Quiterio <edwinegq@outlook.com>
2024-08-06 23:25:12 -0600

    Changes in the DB, add new Entities

--- SCHEMA.sql
+++ SCHEMA.sql
@@ -1,36 +1,76 @@
 /* DATABASE FOR SERVER: MySQL*/
-CREATE DATABASE sy_admin_db;
+-- CREATE DATABASE sy_admin_db;
 
-USE sy_admin_db;
+-- USE sy_admin_db;
+/*----Nueva al 06/08/2024---*/
+CREATE TABLE administration(
+	`id` INT AUTO_INCREMENT NOT NULL,
+    `enterprise` VARCHAR(250) NOT NULL,
+    `capital_mount` DOUBLE NOT NULL,
+    `business_line` VARCHAR(250) NOT NULL,
+    `mission` TEXT NULL,
+    `vision` TEXT NULL,
+    `history` TEXT NULL,
+	`date` DATETIME NOT NULL,
+    CONSTRAINT pk_id_administration PRIMARY KEY(`id`)
+);
+
+/*----Nueva al 06/08/2024---*/
+CREATE TABLE branch(
+	`id` INT AUTO_INCREMENT NOT NULL,
+    `administration_id` INT NOT NULL,
+    `capital_mount` DOUBLE NOT NULL,
+    `phone_number` VARCHAR(50) NULL,
+    `email` VARCHAR(200) NULL,
+    `state` VARCHAR(100) NOT NULL,
+    `city` VARCHAR(250) NOT NULL,
+    `municipality` VARCHAR(100) NOT NULL,
+    `street` TEXT NULL,
+	`out_number` VARCHAR(10) NULL,
+    `in_number` VARCHAR(10) NULL,
+    `date` DATETIME NOT NULL,
+    CONSTRAINT pk_id_branch PRIMARY KEY(`id`),
+    CONSTRAINT fk_administration_id_branch FOREIGN KEY(`administration_id`)
+		REFERENCES administration(`id`)
+);
 
 CREATE TABLE product(
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(250) NOT NULL,
-    price DOUBLE NOT NULL,
-    description TEXT NULL,
-    stock INT NOT NULL,
-    unit VARCHAR(10) NOT NULL,
-    expiration_date DATE NULL,
-    active BOOLEAN DEFAULT TRUE NOT NULL,
-    var_code VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_product_id PRIMARY KEY (id)
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `branch_id` INT NOT NULL,
+    `name` VARCHAR(250) NOT NULL,
+    `in_price` DOUBLE NOT NULL,
+    `out_price` DOUBLE NOT NULL,
+    `wholesale_price` DOUBLE NOT NULL,
+    `description` TEXT NULL,
+    `stock` INT NOT NULL,
+    `min_stock` INT NULL,
+    `unit` VARCHAR(10) NOT NULL,
+    `expiration_date` DATE NULL,
+    `active` BOOLEAN DEFAULT TRUE NOT NULL,
+    `var_code` VARCHAR(100) NOT NULL,
+    CONSTRAINT `pk_product_id` PRIMARY KEY (`id`),
+    CONSTRAINT `fk_branch_id_product` FOREIGN KEY(`branch_id`) 
+		REFERENCES branch(`id`)
 );
 
 CREATE TABLE customer(
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    surname VARCHAR(150) NOT NULL,
-    birthday DATE NOT NULL,
-    age INT NULL,
-    phone_number VARCHAR(20) NULL,
-    email VARCHAR(250) NULL,
-    address TEXT NULL,
-    no_sales INT NULL,
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `branch_id` INT NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `surname` VARCHAR(150) NOT NULL,
+    `birthday` DATE NOT NULL,
+    `age` INT NULL,
+    `phone_number` VARCHAR(20) NULL,
+    `email` VARCHAR(250) NULL,
+    `address` TEXT NULL,
+    `no_sales` INT NULL,
     active BOOLEAN DEFAULT TRUE NOT NULL,
-    CONSTRAINT pk_customer_id PRIMARY KEY (id)
+    CONSTRAINT `pk_customer_id` PRIMARY KEY (`id`),
+    CONSTRAINT `fk_branch_id_customer` FOREIGN KEY(`branch_id`) 
+		REFERENCES branch(`id`)
 );
 
-INSERT INTO customer(`id`, `name`, `suername`, `birthday`, `age`, `phone_number`, `email`, `address`, `no_sales`, `active`) 
+INSERT INTO customer(`id`, `name`, `surname`, `birthday`, `age`, `phone_number`, `email`, `address`, `no_sales`, `active`) 
 VALUES(DEFAULT, 'All', 'Generic', '2000-07-31', null, null, null, null, 0, DEFAULT);
 
 CREATE TABLE provider(
@@ -45,29 +85,51 @@ CREATE TABLE provider(
     CONSTRAINT pk_employee_id PRIMARY KEY (id)
 );
 
+/*----Nueva al 06/08/2024---*/
+CREATE TABLE user_type(
+	`id` INT AUTO_INCREMENT NOT NULL,
+    `type` VARCHAR(100) NOT NULL,
+    `description` VARCHAR(250) NOT NULL,
+    `create_at` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT `pk_id_user_type` PRIMARY KEY(`id`)
+);
+
+INSERT INTO user_type(`id`, `type`, `description`, `create_at`)
+	VALUES(DEFAULT, 'SUPERADMIN', 'Administrador de sistema', DEFAULT);
+INSERT INTO user_type(`id`, `type`, `create_at`)
+	VALUES(DEFAULT, 'ADMIN','Administrador de sucursal', DEFAULT);
+INSERT INTO user_type(`id`, `type`, `create_at`)
+	VALUES(DEFAULT, 'REGISTER','Cajero', DEFAULT);
+
+
 CREATE TABLE user(
-    id INT AUTO_INCREMENT NOT NULL,
-    user_name VARCHAR(50) NULL,
-    user_password VARCHAR(100) NOT NULL,
-    type VARCHAR(50) NOT NULL,
-    CONSTRAINT pk_user_id PRIMARY KEY (id) 
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `user_type_id` INT NOT NULL,
+    `user_name` VARCHAR(50) NULL,
+    `user_password` VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_user_id PRIMARY KEY (id), 
+    CONSTRAINT `fk_user_type_id_user` FOREIGN KEY(`user_type_id`) 
+		REFERENCES user_type(`id`)
 );
 
 CREATE TABLE employee(
-    id INT AUTO_INCREMENT NOT NULL,
-    user_id INT NULL,
-    name VARCHAR(100) NOT NULL,
-    surname VARCHAR(150) NOT NULL,
-    salary DOUBLE NOT NULL,
-    birthday DATE NOT NULL,
-    age INT NULL,
-    phone_number VARCHAR(20) NULL,
-    email VARCHAR(250) NULL,
-    address TEXT NULL,
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `user_id` INT NULL,
+    `branch_id` INT NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `surname` VARCHAR(150) NOT NULL,
+    `salary` DOUBLE NOT NULL,
+    `birthday` DATE NOT NULL,
+    `age` INT NULL,
+    `phone_number` VARCHAR(20) NULL,
+    `email` VARCHAR(250) NULL,
+    `address` TEXT NULL,
     active BOOLEAN NOT NULL,
-    CONSTRAINT pk_employee_id PRIMARY KEY (id),
-    CONSTRAINT fk_employe_user_id FOREIGN KEY (user_id)    
-        REFERENCES user(id) 
+    CONSTRAINT `pk_employee_id` PRIMARY KEY (`id`),
+    CONSTRAINT `fk_employe_user_id` FOREIGN KEY (`user_id`)    
+        REFERENCES user(`id`),
+	CONSTRAINT `fk_branch_id_employee` FOREIGN KEY(`branch_id`) 
+		REFERENCES branch(`id`)
 );
 
 CREATE TABLE pay_method(
@@ -97,6 +159,19 @@ INSERT INTO ticket (`id`, `method`, `detail`)
 VALUEs(DEFAULT, 'I', 'Impreso');
 INSERT INTO ticket (`id`, `method`, `detail`) 
 VALUEs(DEFAULT, 'E', 'Correo electrónico');
+
+/*----Nueva al 06/08/2024---*/
+CREATE TABLE register(
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `branch_id` INT NOT NULL,
+    `initial_mount` DOUBLE NOT NULL,
+    `initial_date` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `close_mount` DOUBLE NOT NULL,
+    `close_date` DATETIME NOT NULL,
+    CONSTRAINT `pk_id_register` PRIMARY KEY(`id`), 
+    CONSTRAINT `fk_branch_id_register` FOREIGN KEY(`branch_id`)
+		REFERENCES branch(`id`)
+);
 
 CREATE TABLE sale(
     `id` INT AUTO_INCREMENT NOT NULL,
@@ -135,17 +210,20 @@ CREATE TABLE sale_item(
 );
 
 CREATE TABLE purchase(
-    id INT AUTO_INCREMENT NOT NULL,
-    employee_id INT NOT NULL,
-    provider_id INT NOT NULL,
-    total DOUBLE NOT NULL,
-    pay_method CHAR(1) NOT NULL,
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `employee_id` INT NOT NULL,
+    `provider_id` INT NOT NULL,
+    `branch_id` INT NOT NULL,
+    `total` DOUBLE NOT NULL,
+    `pay_method` CHAR(1) NOT NULL,
     date DATETIME NOT NULL,
     CONSTRAINT pk_purchase_id PRIMARY KEY (id),
     CONSTRAINT fk_purchase_employee_id FOREIGN KEY (employee_id) 
         REFERENCES employee (id),
     CONSTRAINT fk_purchase_provider_id FOREIGN KEY (provider_id)
-        REFERENCES provider (id)
+        REFERENCES provider (id),
+	CONSTRAINT `fk_branch_id_purchase` FOREIGN KEY(`branch_id`) 
+		REFERENCES branch(`id`)
 );
 
 CREATE TABLE purchase_item(
